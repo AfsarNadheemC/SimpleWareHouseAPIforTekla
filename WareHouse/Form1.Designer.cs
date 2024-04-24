446ef64b8545c5633c07e862fd4ece0d5218683e
Afsar Nadheem C <afsar17052002@gmail.com>
2024-04-24 22:36:54 +0530

    First Commit

---
+++ WareHouse/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WareHouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Height = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(251, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(251, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 45);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(251, 178);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(281, 45);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(252, 268);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(281, 45);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Height
+            // 
+            this.Height.AutoSize = true;
+            this.Height.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Height.Location = new System.Drawing.Point(45, 86);
+            this.Height.Name = "Height";
+            this.Height.Size = new System.Drawing.Size(133, 43);
+            this.Height.TabIndex = 4;
+            this.Height.Text = "Height";
+            this.Height.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 43);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Width";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 43);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Length";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 529);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Height);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Height;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WareHouse/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TSM = Tekla.Structures.Model;
+using TSG = Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+
+namespace WareHouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TSM.Model model = new TSM.Model();
+            if (model.GetConnectionStatus())
+            {
+                int length = Convert.ToInt32(textBox3.Text);
+                int width = Convert.ToInt32(textBox4.Text);
+                int height = Convert.ToInt32(textBox1.Text);
+
+                int lengthPart = length / 3000;
+                
+                double lengthBalance = (length % 3000)/lengthPart;
+
+                double lengthGap = 3000+lengthBalance;
+
+                for (double i = 0; i <= lengthPart; i ++ )
+                {
+                    TSG.Point point = new TSG.Point(0,i*lengthGap,0);
+                    TSG.Point point1 = new TSG.Point(0,i*lengthGap,height);
+
+                    TSM.Beam beam = new TSM.Beam(point,point1);
+
+                    beam.Material.MaterialString = "IS2062";
+                    beam.Profile.ProfileString = "ISMB400";
+                    beam.Class = "7";
+                  
+
+                    beam.Insert();
+                    model.CommitChanges();
+
+                    TSG.Point point2 = new TSG.Point(width, i * lengthGap, 0);
+                    TSG.Point point3 = new TSG.Point(width, i * lengthGap, height);
+
+                    TSM.Beam beam1 = new TSM.Beam(point2, point3);
+
+                    beam1.Material.MaterialString = "IS2062";
+                    beam1.Profile.ProfileString = "ISMB400";
+                    beam1.Class = "7";
+                    
+
+                    beam1.Insert();
+                    model.CommitChanges();
+
+                    TSG.Point point4 = new TSG.Point(0, i * lengthGap, height);
+                    TSG.Point point5 = new TSG.Point(width/2, i * lengthGap, height+(height/4));
+
+                    TSM.Beam beam2 = new TSM.Beam(point4, point5);
+
+                    beam2.Material.MaterialString = "IS2062";
+                    beam2.Profile.ProfileString = "ISMB400";
+                    beam2.Class = "6";
+
+                    beam2.Insert();
+                    model.CommitChanges();
+
+                    TSG.Point point6 = new TSG.Point(width/2, i * lengthGap, height + (height / 4));
+                    TSG.Point point7 = new TSG.Point(width, i * lengthGap, height);
+
+                    TSM.Beam beam3 = new TSM.Beam(point6, point7);
+
+                    beam3.Material.MaterialString = "IS2062";
+                    beam3.Profile.ProfileString = "ISMB400";
+                    beam3.Class = "6";
+
+                    beam3.Insert();
+                    model.CommitChanges();
+                    
+                }
+
+                TSG.Point point8 = new TSG.Point(width/2,0, height + (height / 4));
+                TSG.Point point9 = new TSG.Point(width / 2, length, height + (height / 4));
+
+                TSM.Beam beam4 = new TSM.Beam(point8,point9);
+
+                beam4.Material.MaterialString = "IS2062";
+                beam4.Profile.ProfileString = "ISMB400";
+                beam4.Class = "5";
+
+                beam4.Insert();
+                model.CommitChanges();
+
+                TSG.Point point10 = new TSG.Point(0, 0, height );
+                TSG.Point point11 = new TSG.Point(0, length, height );
+
+                TSM.Beam beam5 = new TSM.Beam(point10, point11);
+
+                beam5.Material.MaterialString = "IS2062";
+                beam5.Profile.ProfileString = "ISMB400";
+                beam5.Class = "5";
+
+                beam5.Insert();
+                model.CommitChanges();
+
+                TSG.Point point12 = new TSG.Point(width , 0, height);
+                TSG.Point point13 = new TSG.Point(width , length, height);
+
+                TSM.Beam beam6 = new TSM.Beam(point12, point13);
+
+                beam6.Material.MaterialString = "IS2062";
+                beam6.Profile.ProfileString = "ISMB400";
+                beam6.Class = "5";
+
+                beam6.Insert();
+                model.CommitChanges();
+
+                TSG.Point point14 = new TSG.Point(0, 0, height);
+                TSG.Point point15 = new TSG.Point(width, 0, height);
+
+                TSM.Beam beam7 = new TSM.Beam(point14, point15);
+
+                beam7.Material.MaterialString = "IS2062";
+                beam7.Profile.ProfileString = "ISMB400";
+                beam7.Class = "6";
+
+                beam7.Insert();
+                model.CommitChanges();
+
+                TSG.Point point16 = new TSG.Point(0, length, height);
+                TSG.Point point17 = new TSG.Point(width, length, height);
+
+                TSM.Beam beam8 = new TSM.Beam(point12, point13);
+
+                beam8.Material.MaterialString = "IS2062";
+                beam8.Profile.ProfileString = "ISMB400";
+                beam8.Class = "6";
+
+                beam8.Insert();
+                model.CommitChanges();
+
+
+            }
+        }
+    }
+}
